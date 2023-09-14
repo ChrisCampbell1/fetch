ea5bd20d7daf4dc3a46871f1b719330e0c712a34
Chris Campbell <christopherecampbell@gmail.com>
2023-09-14 16:55:11 -0600

    set up react router dom

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// npm modules
+import { Routes, Route } from "react-router-dom"
+
+// page components
+
+// components
+
+// services
+
+//styles
+
+export default function App() {
+  return (
+    <>
+      <Routes>
+        
+      </Routes>
+    </>
+  )
+}
